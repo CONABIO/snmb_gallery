50791b55ca77367d73cfb38df921f29c230c646f
jdonlucas <jdonlucas93@gmail.com>
2022-06-30 11:57:47 -0500

    added skeleton to gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import utils from './utils';
-
 import Box from '@mui/material/Box';
 import Masonry from '@mui/lab/Masonry';
 import Pagination from '@mui/material/Pagination';
+import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
 import {
   TextField,
@@ -18,14 +18,15 @@ function App() {
 
   const [imagesList, setList] = useState([])
   const [categoriesList, setCatList] = useState([])
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [maxPages, setMaxPages] = useState(0);
   const [catValue, setCatValue] = useState(null);
   const [image, setImage] = useState(null);
+  const [loadedImg, changeStatusImg] = useState(false);
   const [showViewer, toggleViewer] = useState(false);
 
   useEffect(() => {
-    getImages(page * 12);
+    getImages((page - 1) * 12);
   }, [page])
 
   const getPagesNumber = async () => {
@@ -38,15 +39,13 @@ function App() {
   }
 
   const handlePageChange = (e, value) => {
-    if (catValue && catValue.length) {
-
-    } else {
-      getImages(page * 12);
+      changeStatusImg(false)
+      getImages((page - 1) * 12);
       setPage(value);
-    }
   }
 
   const getImages = async (pag) => {
+    console.log(pag)
     if (catValue && Object.keys(catValue).length) {
       let res = await utils.getCategoryImages(catValue.id, pag);
       let pageQuery = await utils.queryZendro(`
@@ -71,7 +70,7 @@ function App() {
 
 
   const getCategories = async () => {
-    if (categoriesList.length == 0) {
+    if (categoriesList.length === 0) {
       let res = await utils.queryZendro(`
       query {
         categories(pagination: {limit:0}, search: {
@@ -89,20 +88,29 @@ function App() {
     `);
       let catList = res.data.data.categories.filter(item => item.category_annotationsFilter.length > 0)
       catList = catList.map(item => { return { label: item.name, id: item.id } });
-
+      catList = catList.sort((a,b) => {
+        if (a.label > b.label) {
+          return 1;
+        }
+        if (a.label < b.label) {
+          return -1;
+        }
+        return 0;
+      } )
       setCatList(catList);
     }
   }
 
   const filterByCategory = async () => {
-    setPage(0);
-    getImages(page);
+    changeStatusImg(false)
+    setPage(1);
+    getImages(0);
   }
 
   const clearFilters = async () => {
-    setPage(0);
+    setPage(1);
     setCatValue(null);
-    let images = await utils.getImages(page);
+    let images = await utils.getImages(0);
     setList(images.data.data.images);
     getPagesNumber();
     getCategories();
@@ -113,10 +121,15 @@ function App() {
     toggleViewer(!showViewer)
   }
 
+  const removeSkeleton = (idx,listLength) => {
+    if(idx === listLength - 1)
+      changeStatusImg(true)
+  }
+
   return (
     <>
       <div className='main-container'>
-        <Box m="auto">
+        <div>
           <div className='search-box-filter'>
             <Autocomplete
               disablePortal
@@ -133,20 +146,37 @@ function App() {
               renderInput={(params) => <TextField {...params} label="Filtrar por categoría" />}
             />
             <Button variant="text" className='filter-button' onClick={filterByCategory}>Filtrar</Button>
-            <Button variant="text" className='filter-button' onClick={clearFilters}>Limpiar filtros</Button>
+            <Button variant="text" className='filter-button' onClick={clearFilters}>Limpiar filtro</Button>
           </div>
-          <Box m="auto" sx={{ width: 1000 }}>
-            <Masonry columns={4} spacing={2}>
+          <div>
+            <Masonry 
+              className='masonry-box'
+              columns={4}
+              spacing={2}
+              defaultHeight={650}
+              defaultColumns={4}
+              defaultSpacing={2}>
               {imagesList.map((image, index) => (
-                <img className='image-item' key={index} src={image.url} alt={image.id} onClick={() => setViewer(image.url)} />
+                <React.Fragment key={index}>
+                  {!loadedImg && <Skeleton variant="rectangular" animation="wave" className='skeleton-height' />}
+                  <img 
+                    className='image-item' 
+                    style={{
+                      display: `${loadedImg ? 'block' : 'none'}` 
+                    }}
+                    src={image.url}
+                    alt={image.id}
+                    onLoad={() => removeSkeleton(index,imagesList.length)}
+                    onClick={() => setViewer(image.url)} /> 
+                </React.Fragment>
               ))}
             </Masonry>
-          </Box>
+          </div>
           <Stack justifyContent="center"
             alignItems="center" spacing={2}>
-            <Pagination count={maxPages} onChange={handlePageChange} showFirstButton showLastButton />
+            <Pagination count={maxPages} page={page} onChange={handlePageChange} showFirstButton showLastButton />
           </Stack>
-        </Box>
+        </div>
         {showViewer && 
         <div className='image-viewer-container'>
           <div className='close-button' onClick={() => toggleViewer(!showViewer)}>Cerrar</div>
