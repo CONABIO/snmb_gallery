f86071af3b18c6eef24d687d35165f36fe68d9ec
jdonlucas <jdonlucas93@gmail.com>
2022-07-08 12:12:39 -0500

    added production url

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const graphqlUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/graphql" : "";
+const graphqlUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/graphql" : "https://snmb.conabio.gob.mx/graphqlzendro/graphql";
 export default {
     getImages(pag) {
         return axios({
